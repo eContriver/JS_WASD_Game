30f0777d7e0fa4946421bf11f06174ce59de2076
Training <training@econtriver.com>
2021-01-07 09:01:22 -0700

    Adds the AssetMgr but there seems to be a problem with the script load as a 2s delay is reporting seedradom is not available

--- game.js
+++ game.js
@@ -10,34 +10,148 @@ var maxYWorld = 640 * 80;
 var minXWorld = 0;
 var maxXWorld = 1040 * 80;
 
-// document.addEventListener("DOMContentLoaded" - the whole document (HTML) has been loaded.
-// window.addEventListener("load" - the whole document and its resources (e.g. images, iframes, scripts) have been loaded.
-//   i.e. window.onload
-document.addEventListener("DOMContentLoaded", function(event) {
-    loadScript("seedrandom.js");
-    visibleCanvas = document.getElementById('game');
-    visibleCanvas.style.backgroundColor = "lightgray";
-    dotYWorld = (minYWorld + 320);
-    dotXWorld = (minXWorld + 520);
-    // dotYWorld = (maxYWorld - 320);
-    // dotXWorld = (maxXWorld - 520);
-    document.addEventListener("keydown", move);
-    context = visibleCanvas.getContext('2d');
-    dotImage = new Image();
-    dotImage.src = 'dot.png';
-    dotImage.onload = function() {
-        context.drawImage(dotImage, dotXWorld, dotYWorld);
-    }
+const ScriptAssets = Object.freeze({
+    'seedrandom': 'seedrandom.js'
+});
+const ImageAssets = Object.freeze({
+    'dot': 'dot.png'
 });
 
-function loadScript(file) {
-    var scriptTag = document.createElement('script');
-    scriptTag.setAttribute("type", "text/javascript");
-    scriptTag.setAttribute("src", file);
-    document.getElementsByTagName("head")[0].appendChild(scriptTag);
+class AssetMgr {
+    constructor() {
+        this.imagesPending = [];
+        this.imagesLoaded = [];
+        this.imageObjects = new Map();
+        this.imageCallbacks = new Map();
+        this.scriptsPending = [];
+        this.scriptsLoaded = [];
+        this.scriptCallbacks = new Map();
+    }
+    addScripts(scripts) {
+        scripts.forEach((function(script){
+            if (this.scriptsLoaded.includes(script)) return;
+            if (this.scriptsPending.includes(script)) return;
+            this.scriptsPending.push(script);
+            loadScript(script, (function() {
+                const index = this.scriptsPending.indexOf(script);
+                this.scriptsLoaded.push(script); // let it exist in both briefly
+                this.scriptsPending.splice(index, 1); // remove it
+                this.runScriptCallbacks(script); // run callbacks as they were
+            }).bind(this));
+        }).bind(this));
+    }
+    onScriptLoad(script, func) {
+        this.scriptCallbacks.set(script, func);
+        this.runScriptCallbacks(script); // run callbacks now in case it was loaded
+    }
+    runScriptCallbacks(script) {
+        if (this.scriptCallbacks.has(script)) {
+            let func = this.scriptCallbacks.get(script); // and run it
+            func(script);
+        }
+    }
+    addImages(srcs) {
+        srcs.forEach(function(src){
+            if (this.imagesLoaded.includes(src)) return;
+            if (this.imagesPending.includes(src)) return;
+            this.imagesPending.push(src);
+            this.loadImage(src, function() {
+                const index = this.imagesPending.indexOf(src);
+                this.imagesLoaded.push(src); // let it exist in both briefly
+                this.imagesPending.splice(index, 1); // remove it
+                this.runImageCallbacks(src);
+            }.bind(this));
+        }.bind(this));
+    }
+    onImageLoad(src, func) {
+        this.imageCallbacks.set(src, func);
+        this.runImageCallbacks(src);
+    }
+    runImageCallbacks(src) {
+        let func = this.imageCallbacks.get(src); // and run it
+        func(this.imageObjects.get(src));
+    }
+    loadImage(image, func) {
+        let loader = new Image();
+        loader.src = image;
+        loader.onload = func;
+        this.imageObjects.set(image, loader);
+    }
 }
 
-function move(event) {
+function sleep(durationMs){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + durationMs){ /* noop */ } 
+}
+
+function loadScript(file, func) {
+    sleep(2000);
+    let element = document.createElement('script');
+    element.type = 'text/javascript';
+    element.src = file;
+    element.addEventListener('load', func);
+    document.getElementsByTagName("head")[0].appendChild(element);
+}
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    midPoint(b) {
+        return new Point((b.x - this.x) / 2, (b.y - this.y) / 2);
+    }
+}
+
+class Character {
+    constructor(location) {
+        this.location = location;
+        this.image = null;
+    }
+}
+
+class World {
+    constructor(max) {
+        this.max = max;
+        this.min = new Point(0, 0);
+    }
+
+    center() {
+        return this.min.midPoint(this.max);
+    }
+}
+
+class Game {
+    constructor() {
+        // only add member variables if the game owns the object
+    }
+    run(assetMgr) {
+        let world = new World(new Point(640 * 80, 1040 * 80));
+        visibleCanvas = document.getElementById('game');
+        visibleCanvas.style.backgroundColor = "lightgray";
+        // Set character starting location 
+        let dot = new Character(world.center()); // middle
+        //let dot = new Character(world.minX + 520, world.minY + 320); // min boundary
+        //let dot = new Character(world.maxX - 520, world.maxY - 320); // max boundary
+        // TODO: Move and draw should be decoupled
+        assetMgr.onScriptLoad(ScriptAssets.seedrandom, function(script) {
+            let keyHandler = function(event) {
+                move(event, assetMgr);
+            };
+            document.addEventListener("keydown", keyHandler); // TODO: move needs to get dot, 
+        });
+        // do we bind and add move to Char or how to add key bindign, that moves char, from hi level
+        context = visibleCanvas.getContext('2d');
+        // TODO: Need assetMgr with after load...
+        assetMgr.onImageLoad(ImageAssets.dot, function(image) {
+            dot.image = image;
+            context.drawImage(dot.image, dot.location.x, dot.location.y);
+        });
+    }
+}
+
+
+function move(event, assetMgr) {
     context.clearRect(0, 0, visibleCanvas.width, visibleCanvas.height);
     var movementAmount = 10;
     switch (event.key) {
@@ -54,6 +168,7 @@ function move(event) {
             dotXWorld += movementAmount;
             break;
     }
+    let dotImage = assetMgr.imageObjects.get(ImageAssets.dot);
     dotYWorld = (dotYWorld + dotImage.height > maxYWorld) ? maxYWorld - dotImage.height : dotYWorld;
     dotYWorld = (dotYWorld < minYWorld) ? minYWorld : dotYWorld;
     dotXWorld = (dotXWorld + dotImage.width > maxXWorld) ? maxXWorld - dotImage.width : dotXWorld;
@@ -81,7 +196,7 @@ function move(event) {
     generateRandom(canvasXWorld, canvasXWorld + visibleCanvas.width, canvasYWorld, canvasYWorld + visibleCanvas.height);
     console.log("Moving to: " + dotXWorld + "," + dotYWorld);
     context.drawImage(dotImage, dotXCanvas, dotYCanvas);
-    event.preventDefault(); // prevents arrows from scrolling
+    //event.preventDefault(); // prevents (F5 and) arrows from scrolling (with wasd we don't need this now)
 }
 
 function generateRandom(xStart, xEnd, yStart, yEnd) {
@@ -104,30 +219,30 @@ function generateRandom(xStart, xEnd, yStart, yEnd) {
             var b = ((rand >>> 2) % 2) * 255;
             var size = (scaleFactor * (((rand >>> 3) % 4) + 1)) / 4;
             var a = 255;
-            colorPixel2(x-xStart, y-yStart, size, r, g, b, a);
+            colorPixel(x-xStart, y-yStart, size, r, g, b, a);
         }
     }
     // console.timeEnd(generateRandom.name)
 }
 
-function PRNG(seed) { 
-    //console.log("Seed: '" + seed + "'");
-    var a = 1103515245;
-    var c = 12345;
-    var m = 2**31;
-    var random = (a * seed + c) % m;
-    // console.log("Random: '" + random + "'");
-    return random;
-}
+// function PRNG(seed) { 
+//     //console.log("Seed: '" + seed + "'");
+//     var a = 1103515245;
+//     var c = 12345;
+//     var m = 2**31;
+//     var random = (a * seed + c) % m;
+//     // console.log("Random: '" + random + "'");
+//     return random;
+// }
 
 // Took over 300 ms to generate for 1040-40 x 640-40
-function colorPixel2(x, y, size, r, g, b, a) {
+function colorPixel(x, y, size, r, g, b, a) {
     context.fillStyle = "rgba(" + r + "," + g + "," + b + "," + (a / 255) + ")";
     context.fillRect(x, y, size, size);
 }
 
 /* Took over 2 seconds to generate for 1040-40 x 640-40
-function colorPixel(x, y) {
+function colorPixel2(x, y) {
     var id = context.createImageData(1,1);
     var d = id.data;
     d[0]   = 255; // r
@@ -136,3 +251,18 @@ function colorPixel(x, y) {
     d[3]   = 255; // a
     context.putImageData( id, x, y );  
 }//*/
+
+function main() {
+    let assetMgr = new AssetMgr();
+    assetMgr.addScripts(Object.values(ScriptAssets));
+    assetMgr.addImages(Object.values(ImageAssets));
+    // document.addEventListener("DOMContentLoaded" - the whole document (HTML) has been loaded.
+    // window.addEventListener("load" - the whole document and its resources (e.g. images, iframes, scripts) have been loaded.
+    //   i.e. window.onload
+    document.addEventListener("DOMContentLoaded", function(event) {
+        let game = new Game();
+        game.run(assetMgr);
+    });
+}
+
+main();
